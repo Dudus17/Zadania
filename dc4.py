24349959036b0f6b4efe0396e176bd7dc3c8f3ec
Dudus <dudus@iMac-Dudus.local>
2023-10-02 12:21:30 +0200

    dodaję zmiany w zadaniu 4

--- dc4.py
+++ dc4.py
@@ -1,18 +1,21 @@
 imię = input("Jak masz na imię?\n")
-print("Witaj",imię, "!Bardzo się cieszę, że jesteś tutaj ze mną. Jest piękny słoneczny dzień, jestem szczęśliwa,\nponieważ uczę się programowania i coraz bardziej mi się to podoba!")
+print("Witaj",imię,"\nBardzo się cieszę, że jesteś tutaj ze mną. Jest piękny słoneczny dzień, jestem szczęśliwa,\nponieważ uczę się programowania i coraz bardziej mi się to podoba!")
 samopoczucie = input("A Ty? Jak się dzisiaj czujesz? ")
-print("Twoja odpowiedź to:", samopoczucie.upper())
+print("\nTwoja odpowiedź to:", samopoczucie.upper(), "\n")
 znaki=len(samopoczucie)
-if znaki == 1:
-    print("Użyłeś w swojej wypowiedzi:", znaki, "znak")
-if znaki >= 1 and znaki <= 4 and znaki >=22 and znaki <= 24:
-    print ("Użyłeś w swojej wypowiedzi:\n", znaki, "znaki")
-if znaki >= 5:
-    print ("Użyłeś w swojej wypowiedzi:\n", znaki, "znaków")
 slowa = len(samopoczucie.split())
-if slowa == 1:
-    print (slowa, "słowo")
-if slowa >= 2 and znaki <=4:
-    print (slowa,"słowa")
-if znaki >= 5:
-    print (slowa, "słów")
+
+from prettytable import PrettyTable
+
+# Tworzenie obiektu PrettyTable
+table = PrettyTable()
+
+# Definiowanie kolumn
+table.field_names = ["Kategoria", "Output"]
+
+# Dodawanie danych do tabeli
+table.add_row(["Użyłeś słów", slowa])
+table.add_row(["Użyłeś znaków", znaki])
+
+# Wyświetlenie tabeli
+print(table)
