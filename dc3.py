84668d0900bd0db31827bfaf9eae64633b79c785
Dudus <dudus@iMac-Dudus.local>
2023-10-02 10:42:30 +0200

    Dodałam zmiany w dc3

--- dc3.py
+++ dc3.py
@@ -1,22 +1,23 @@
 print ("Hello")
-symbol = input ("Jakie zadanie matematyczne chcesz wykonać? Dozwolone znaki to:\n +\n -\n *\n / \n\n")
+symbol = input ("Jakie zadanie matematyczne chcesz wykonać? Dozwolone znaki to:\n +\n -\n *\n / \n\n").strip()
 if symbol != "/" and symbol != "*" and symbol != "-" and symbol != "+":
-    print("Symbol",symbol,"jest niedozwolony, dozwolone znaki to: +, -, *, /")
+    print("Symbol {} jest niedozwolony, dozwolone znaki to: +, -, *, /".format(symbol))
 else:
     zmienna = int(input ("Podaj zmienną A: "))
     zmiennaB = float(input ("Podaj zmienną B: "))
 
     if symbol == "*":
-        result1 = (zmienna * zmiennaB)
-        print("Wynik:", result1)
-    if symbol == "+":
-        result3 = (zmienna + zmiennaB)
-        print("Wynik:", result3)
-    if symbol == "-":
-        result4 = (zmienna - zmiennaB)
-        print("Wynik:", result4)
-    if symbol == "/" and zmiennaB == 0:
+        result = (zmienna * zmiennaB)
+        print("Wynik:", result)
+    elif symbol == "+":
+        result = (zmienna + zmiennaB)
+        print("Wynik:", result)
+    elif symbol == "-":
+        result = (zmienna - zmiennaB)
+        print("Wynik:", result)
+    elif symbol == "/":
+        if zmiennaB == 0:
             print("Pamiętaj cholero nie dziel przez zero! :)")
-    else:
+        else:
             result = (zmienna / zmiennaB)
             print("Wynik:", result)
